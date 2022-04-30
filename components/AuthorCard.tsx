c945d75ed64dcdb81c0e8c290a6e3e3edef590e0
Mohamed Achaq <motriyanox@gmail.com>
2022-04-30 16:45:56 +0000

    Update AuthorCard.tsx

--- components/AuthorCard.tsx
+++ components/AuthorCard.tsx
@@ -3,7 +3,7 @@ import achaq from '../assets/achaq.jpg'
 
 const AuthorCard = () => {
   return (
-    <div className="mt-24 flex flex-col items-center justify-center gap-4 rounded-xl bg-gray-200 p-6 dark:bg-zinc-800 sm:flex-row sm:gap-12">
+    <div className="mt-24 flex flex-col items-center justify-center gap-4 rounded-xl bg-gray-100 p-6 dark:bg-zinc-900 sm:flex-row sm:gap-12">
       <div className="justify-cente flex w-1/2 flex-col items-center md:w-full">
         <Image
           src={achaq}
