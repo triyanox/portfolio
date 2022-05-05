97380b3544c5243becc76c3403d8ae396411d672
Mohamed Achaq <motriyanox@gmail.com>
2022-05-05 02:05:32 +0000

    Update Intro.tsx

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,5 +1,5 @@
 import { Button, ButtonWhite } from './EmailButton'
-import achaq from '../assets/achaq.jpg'
+import achaq from '../assets/achaq.png'
 import Image from 'next/image'
 import Link from 'next/link'
 const Intro = () => {
