4a6e63dbecb8470448d3b3e747c99d53f98a646f
Mohamed Achaq <motriyanox@gmail.com>
2022-05-08 05:47:38 +0100

    Navbar update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,21 @@
 import { useContext } from 'react'
 import { ActiveContext } from '../components/ActiveContext'
 import Toggle from './ThemeToggle'
-import { HiOutlineMenuAlt4 } from 'react-icons/hi'
+import { CgMenuRight } from 'react-icons/cg'
 import { VscChromeClose } from 'react-icons/vsc'
 import { motion } from 'framer-motion'
 const Navbar = () => {
   const { active, setActive } = useContext(ActiveContext)
   return (
-    <section className="fixed top-0 z-50 flex w-full flex-row items-center justify-between bg-white px-8 py-4 dark:bg-black md:px-16">
+    <section className="fixed top-0 z-50 flex w-full flex-row items-center justify-between bg-white px-8 py-4 dark:bg-black md:px-16 bg-opacity-70 backdrop-blur-xl dark:bg-opacity-70 dark:backdrop-blur-xl">
       <motion.button
         whileTap={{ scale: 0.9, rotate: -30, elevation: 10 }}
         transition={{ duration: 0.3, damping: 10 }}
         aria-label="Menu Switch"
         onClick={() => setActive(active === false ? true : false)}
-        className=" text-2xl text-black dark:text-white "
+        className=" text-3xl text-black dark:text-white "
       >
-        {!active ? <HiOutlineMenuAlt4 /> : <VscChromeClose />}
+        {!active ? <CgMenuRight /> : <VscChromeClose />}
       </motion.button>
       <Toggle />
     </section>
