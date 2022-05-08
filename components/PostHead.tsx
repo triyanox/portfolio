50474a0af2faeb83c3c3309fb55949ce08406a4e
Mohamed Achaq <motriyanox@gmail.com>
2022-05-08 05:19:55 +0100

    title color changed

--- components/PostHead.tsx
+++ components/PostHead.tsx
@@ -8,7 +8,7 @@ export function PostHead({ title, date, readTime }: Props) {
   return (
     <div className="flex flex-col items-start pt-14 pb-14">
       <div className="col-span-full lg:col-span-8 lg:col-start-3">
-        <h2 className="text-3xl leading-tight text-black dark:text-white md:text-4xl">
+        <h2 className="text-3xl leading-tight bg-gradient-to-r dark:from-cyan-400 dark:to-green-300 text-transparent bg-clip-text from-cyan-400 to-blue-600 md:text-4xl">
           {title}
         </h2>
         <p className="dark:text-blueGray-500 mt-2 text-lg font-medium text-gray-400">
