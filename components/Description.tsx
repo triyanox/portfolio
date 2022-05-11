6660029c1b31d592725e503813b1b915a6a9449a
Mohamed Achaq <motriyanox@gmail.com>
2022-05-11 18:55:31 +0100

    Fixing the width

--- components/Description.tsx
+++ components/Description.tsx
@@ -29,7 +29,7 @@ export const Description = (props: Description) => {
           stiffness: 100,
           elapsed: 1,
         }}
-        className="flex flex-col items-start justify-center rounded-[30px] bg-gray-50 p-8 dark:bg-zinc-900 "
+        className="flex w-full flex-col items-start justify-center rounded-[30px] bg-gray-50 p-8 dark:bg-zinc-900 "
       >
         <p className="text-2xl font-medium text-gray-600 dark:text-gray-400 ">
           {props.text}
@@ -39,7 +39,7 @@ export const Description = (props: Description) => {
             <p className="mt-4 text-2xl font-medium text-gray-600 dark:text-gray-400 ">
               Tools I'm using :
             </p>
-            <div className=" mt-4 flex flex-row justify-start gap-3  text-3xl text-gray-600 dark:text-gray-400 md:text-3xl">
+            <div className=" mt-4 flex flex-row justify-start gap-3 text-2xl text-gray-600 dark:text-gray-400 md:text-3xl md:text-3xl">
               {props.tools.map((item) => item)}
             </div>
           </div>
