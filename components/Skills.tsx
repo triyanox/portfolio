e88fc368ec2ccbe01d46ba36f1af3cff3ba20898
Mohamed Achaq <motriyanox@gmail.com>
2022-05-16 06:33:48 +0100

    new animation reveal

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,6 +1,6 @@
 import { Description } from './Description'
 import { Option, OptionSelected } from './Option'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {
   Figma,
   Adobeillustrator,
@@ -20,20 +20,88 @@ import {
   Postgresql,
   Go,
 } from './Icons'
+import { motion, useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
 const Skills = () => {
   const [skill, setSkill] = useState('Frontend')
-
+  const controls = useAnimation()
+  const [ref, inView] = useInView()
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible')
+    }
+  }, [controls, inView])
   return (
     <section className="mt-24 flex w-full flex-col justify-center gap-y-4 px-8 md:px-16 ">
-      <div className="flex flex-col items-start justify-center">
+      <motion.div
+        ref={ref}
+        animate={controls}
+        initial="hidden"
+        variants={{
+          visible: {
+            translateX: 0,
+            opacity: 1,
+            transformOrigin: 'bottom',
+            transition: {
+              duration: 0.5,
+              dump: 0.8,
+              stiffness: 100,
+              ease: 'easeInOut',
+            },
+          },
+          hidden: {
+            translateX: -100,
+            opacity: 0,
+            transformOrigin: 'bottom',
+            transition: {
+              duration: 0.5,
+              dump: 0.8,
+              stiffness: 100,
+              ease: 'easeInOut',
+            },
+          },
+        }}
+        className="flex flex-col items-start justify-center"
+      >
         <h2 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-gray-200">
           Skills
         </h2>
         <p className="mb-8 text-lg font-medium text-gray-600 dark:text-gray-400 ">
           My skill set
         </p>
-      </div>
-      <div className="flex w-full flex-col justify-center  gap-4 md:flex-row">
+      </motion.div>
+      <motion.div
+        ref={ref}
+        animate={controls}
+        initial="hidden"
+        variants={{
+          visible: {
+            translateY: 0,
+            scale: 1,
+            opacity: 1,
+            transformOrigin: 'bottom',
+            transition: {
+              duration: 0.5,
+              dump: 0.8,
+              stiffness: 100,
+              ease: 'easeInOut',
+            },
+          },
+          hidden: {
+            translateY: 100,
+            scale: 0.6,
+            opacity: 0.2,
+            transformOrigin: 'bottom',
+            transition: {
+              duration: 0.5,
+              dump: 0.8,
+              stiffness: 100,
+              ease: 'easeInOut',
+            },
+          },
+        }}
+        className="flex w-full flex-col justify-center  gap-4 md:flex-row"
+      >
         <div className="mb-4 flex w-full flex-row items-start justify-start gap-2 gap-x-4 overflow-scroll md:mb-0 md:flex-col md:overflow-hidden ">
           {skill === 'Frontend' ? (
             <OptionSelected
@@ -130,7 +198,7 @@ const Skills = () => {
             />
           )}
         </div>
-      </div>
+      </motion.div>
     </section>
   )
 }
