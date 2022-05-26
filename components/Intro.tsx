4e0d420a86a3bbb099d7648b08ac11674835138f
Mohamed Achaq <motriyanox@gmail.com>
2022-05-26 15:04:29 +0100

    Adding a resume

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -15,8 +15,7 @@ const Intro = () => {
   }, [controls, inView])
   return (
     <section className="relative flex w-full flex-col-reverse items-center  justify-center overflow-hidden px-8 pb-8   md:h-screen md:flex-row md:px-16  md:pb-0">
-      <div className="mt-24 flex w-full flex-col items-start justify-center gap-2 md:mt-0"
-      >
+      <div className="mt-24 flex w-full flex-col items-start justify-center gap-2 md:mt-0">
         <h1 className="text-4xl font-semibold text-black dark:text-white">
           Mohamed Achaq
         </h1>
@@ -29,14 +28,14 @@ const Intro = () => {
           love to discover and learn new technologies.
         </p>
         <div className="flex flex-row items-center justify-center gap-8">
-          <Link href="mailto:contact@achaq.codes" passHref>
+          <Link href="/resume.pdf" passHref>
             <a>
-              <Button text="Mail Me" />
+              <Button text="Resume" />
             </a>
           </Link>
-          <Link href="/blog" passHref>
+          <Link href="mailto:contact@achaq.codes" passHref>
             <a>
-              <ButtonWhite text="The Blog" />
+              <ButtonWhite text="Get in touch" />
             </a>
           </Link>
         </div>
